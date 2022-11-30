c3c0a877ff4fa16cd9e72f3f725748bd2fde0cdc
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-01 01:06:58 +0530

    Factorial logic

---
+++ src/JavaAssignments/Factorial.java
@@ -0,0 +1,17 @@
+package JavaAssignments;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = scan.nextInt();
+        int fact = 1;
+
+        for (int i = 1; i < num + 1; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of the entered number: " + fact);
+    }
+}
