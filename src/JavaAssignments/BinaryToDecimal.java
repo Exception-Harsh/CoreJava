931d9a7094053a3f2a487be52d86c2c27ba4c764
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-12 16:45:04 +0530

    Binary to Decimal

--- src/JavaAssignments/BinaryToDecimal.java
+++ src/JavaAssignments/BinaryToDecimal.java
@@ -8,11 +8,10 @@ public class BinaryToDecimal {
         System.out.print("Enter Binary Number: ");
         long binNum = scan.nextLong();
         long temp = binNum;
-        int decNum = 0, count = 0, i = 0;
+        int decNum = 0, i = 0;
 
         while (temp != 0) {
             long remainder = temp % 10;
-            count++;
             temp /= 10;
             decNum += remainder * Math.pow(2, i++);
         }
