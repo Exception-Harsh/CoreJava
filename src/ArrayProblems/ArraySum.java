8f983bdcc0999e95f8391c6c31aae09004e31d47
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-13 22:11:51 +0530

    Sum of elements of Arrays

---
+++ src/ArrayProblems/ArraySum.java
@@ -0,0 +1,13 @@
+package ArrayProblems;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5};
+        int sum = 0;
+
+        for (int j : arr) {
+            sum += j;
+        }
+        System.out.println("Sum of the elements in array: " + sum);
+    }
+}
