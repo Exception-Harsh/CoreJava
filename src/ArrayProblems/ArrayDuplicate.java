004bd89f52ae6181fcbebbca5992ed81311f2e70
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-14 19:23:49 +0530

    Duplicate values in Array

---
+++ src/ArrayProblems/ArrayDuplicate.java
@@ -0,0 +1,15 @@
+package ArrayProblems;
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+    public static void main(String[] args) {
+        int[] array = {18, 13, 25, 19, 34, 18, 25};
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if ((array[i] == array[j]) && (i != j))
+                    System.out.println("Duplicate value of " + array[j] + " exists at index: " + j);
+            }
+        }
+    }
+}
