cb966756ca9c78836aa768d81ed45ca88952d098
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-17 12:33:23 +0530

    Trying out Basic concepts of Array

---
+++ src/ArrayProblems/ArrayConcepts.java
@@ -0,0 +1,33 @@
+package ArrayProblems;
+import java.util.Scanner;
+
+public class ArrayConcepts {
+    static Scanner scan = new Scanner(System.in);
+
+    static void inputIntArray(int[] array) {
+        System.out.print("Enter " + array.length + " elements in the Array: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+    }
+    static void printIntArray(int[] array) {
+        System.out.print("Elements in the Array: ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the size of Array: ");
+        int size = scan.nextInt();
+        int[] array_1 = new int[size];
+
+        inputIntArray(array_1);
+        printIntArray(array_1);
+
+        int[] array_2;
+        array_2 = array_1;
+        printIntArray(array_2);
+    }
+}
