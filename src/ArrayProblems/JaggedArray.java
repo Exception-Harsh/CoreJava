5cfbbe6904d373b07c4c32bead14370c883d6ae1
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-19 09:12:11 +0530

    Jagged Array Concept

---
+++ src/ArrayProblems/JaggedArray.java
@@ -0,0 +1,29 @@
+package ArrayProblems;
+import java.util.Scanner;
+
+public class JaggedArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] JagArr = new int[3][];
+
+        for (int i = 0; i < JagArr.length; i++) {
+            JagArr[i] = new int[i + 1];
+        }
+
+        System.out.print("Enter Elements in Jagged Array: ");
+        for (int i = 0; i < JagArr.length; i++) {
+            for (int j = 0; j < JagArr[i].length; j++) {
+                JagArr[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Elements in Jagged Array: ");
+        for (int[] intI : JagArr) {
+            for (int intJ : intI) {
+                System.out.print(intJ + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
