b9aa2bf3fee7bb03fb8d74fb339b2a64b4263567
Exception-Harsh <harsh180403singh@gmail.com>
2022-12-27 08:49:09 +0530

    ...

--- src/ArrayProblems/ArrayConcepts.java
+++ src/ArrayProblems/ArrayConcepts.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class ArrayConcepts {
     static Scanner scan = new Scanner(System.in);
-
-    static void inputIntArray(int[] array) {
+    public static void inputIntArray(int[] array) {
         System.out.print("Enter " + array.length + " elements in the Array: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextInt();
         }
     }
-    static void printIntArray(int[] array) {
+    public static void printIntArray(int[] array) {
         System.out.print("Elements in the Array: ");
         for (int j : array) {
             System.out.print(j + " ");
