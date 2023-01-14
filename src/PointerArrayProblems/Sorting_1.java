2c47fef01ad89ff50a074d5b5b565e619cce1fcf
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-01-14 17:23:55 +0530

    Update Sorting_1.java

--- src/PointerArrayProblems/Sorting_1.java
+++ src/PointerArrayProblems/Sorting_1.java
@@ -1,25 +1,10 @@
 package PointerArrayProblems;
 import ArrayProblems.ArrayConcepts;
-
 import java.util.Scanner;
 
 public class Sorting_1 {
 
-    public static void sortZeroesAndOnes_1(int[] arr) {
-        int zeroes = 0;
-        for (int i : arr) {
-            if (i == 0)
-                zeroes++;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (i < zeroes)
-                arr[i] = 0;
-            else
-                arr[i] = 1;
-        }
-    }
-
-    public static void sortZerosAndOnes_2(int[] arr) {
+    public static void sortZerosAndOnes(int[] arr) {
         int i = 0, j = arr.length - 1;
         while (i < j) {
             if (arr[i] == 1 && arr[j] == 0) {
@@ -40,12 +25,8 @@ public class Sorting_1 {
         ArrayConcepts.inputIntArray(array1);
         ArrayConcepts.printIntArray(array1);
 
-        System.out.println("Array after sorting with 1st function: ");
-        sortZeroesAndOnes_1(array1);
-        ArrayConcepts.printIntArray(array1);
-
-        System.out.println("Array after sorting with 2nd function: ");
-        sortZerosAndOnes_2(array1);
+        System.out.println("Array after sorting: ");
+        sortZerosAndOnes(array1);
         ArrayConcepts.printIntArray(array1);
     }
 }
