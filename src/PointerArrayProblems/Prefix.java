5061c816406705b468ed53c10197a3fd41205164
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-01-18 22:00:17 +0530

    Prefix Sum problem
    Creating a new array or replacing the given array by the adding the values of given array from 0 index to the current index.

---
+++ src/PointerArrayProblems/Prefix.java
@@ -0,0 +1,35 @@
+package PointerArrayProblems;
+import ArrayProblems.ArrayConcepts;
+import java.util.Scanner;
+
+public class Prefix {
+
+    public static void replaceArray(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
+        }
+    }
+    public static int[] makePrefixSumArray(int[] arr) {
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length ; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter size of Array: ");
+        int size = scan.nextInt();
+        int[] array = new int[size];
+        ArrayConcepts.inputIntArray(array);
+        ArrayConcepts.printIntArray(array);
+
+        int[] ans = makePrefixSumArray(array);
+        ArrayConcepts.printIntArray(ans);
+        //replacing the elements in array by its prefix sum values
+        replaceArray(array);
+        ArrayConcepts.printIntArray(array);
+    }
+}
