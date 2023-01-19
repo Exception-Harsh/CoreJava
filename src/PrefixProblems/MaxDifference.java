ed72235aebb4d3209ee37cd0a5157b70c750dcd4
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2023-01-19 23:44:52 +0530

    Max Difference between two sub arrays
    Dividing the array in two parts and the size of one part should be taken from the user. Array should be divided in such a way that the difference between the sums of the two sub arrays is max.

---
+++ src/PrefixProblems/MaxDifference.java
@@ -0,0 +1,41 @@
+package PrefixProblems;
+import ArrayProblems.ArrayConcepts;
+import java.util.Scanner;
+
+public class MaxDifference {
+
+    public static int[] makePrefixArray(int[] arr) {
+        int[] prefixArray = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        prefixArray[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefixArray[i] = arr[i] + prefixArray[i - 1];
+        }
+        return prefixArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter size and m: ");
+        int size = scan.nextInt();
+        int m = scan.nextInt();
+
+        int[] array = new int[size];
+        ArrayConcepts.inputIntArray(array);
+        int[] ans = makePrefixArray(array);
+
+        int diff = ans[ans.length - 1] - ans[m - 1];
+        int maxDiff = diff - ans[m - 1];
+        System.out.println("Maximum Difference = " + maxDiff);
+    }
+}
