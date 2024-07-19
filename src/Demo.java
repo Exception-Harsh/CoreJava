b8cc33555ff973416fe40103ef2cf31ccdd63033
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2024-07-19 16:01:24 +0530

    pushing all changes

--- src/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-        String s1 = "Umbrella";
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) == 'e')
-                System.out.println("e is present");
-        }
-    }
-}
