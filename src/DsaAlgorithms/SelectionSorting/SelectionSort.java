53ca2568072cf20c72764833cf6e2c88cb849a62
Exception-Harsh <111991803+Exception-Harsh@users.noreply.github.com>
2024-07-22 19:05:07 +0530

    updated the program with descending order sorting using selection sort

--- src/DsaAlgorithms/SelectionSorting/SelectionSort.java
+++ src/DsaAlgorithms/SelectionSorting/SelectionSort.java
@@ -4,7 +4,7 @@ import OneDimensionalArray.ArrayProblems.ArrayConcepts;
 import java.util.Scanner;
 
 public class SelectionSort {
-    public static void selectionSort(int[] arr) {
+    public static void ascSelectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) { // i represent the current index of the array
             //Used to find the min element in the unsorted array
             int min_index = i;
@@ -18,6 +18,20 @@ public class SelectionSort {
         }
     }
 
+    public static void descSelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) { // i represent the current index of the array
+            //Used to find the max element in the unsorted array
+            int max_index = i;
+            for (int j = i + 1; j < arr.length; j++) { // j represents the unsorted part of the array
+                if (arr[j] > arr[max_index])
+                    max_index = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[max_index];
+            arr[max_index] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter size of array: ");
@@ -26,7 +40,10 @@ public class SelectionSort {
         int[] array = new int[size];
         ArrayConcepts.inputIntArray(array);
 
-        selectionSort(array);
+//        selectionSort(array);
+//        ArrayConcepts.printIntArray(array);
+
+        descSelectionSort(array);
         ArrayConcepts.printIntArray(array);
     }
 }
